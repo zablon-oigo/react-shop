2d7205310e5fe50919234e272ac435156dd87846
zablon <zablonoigo51@gmail.com>
2024-03-27 15:41:03 +0300

    Home Component

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <>
+    <div className="">
+     <div className=""></div>
+     <div className="">
+        <h1 className="">
+            good living <br />Better <span className="">life</span>
+        </h1>
+        <p className="">find you way to the goo living with our furniture.</p>
+     </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
