4e5ebce05a392f1acf45bae26400f1efde243817
zablon <zablonoigo51@gmail.com>
2024-03-27 11:40:58 +0300

    Adding Navbar Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-scroll"
 function Navbar() {
     const[menu,setMenu]=useState(false)
     const toggleMenu=()=>{
@@ -10,7 +11,21 @@ function Navbar() {
   return (
     <>
     <nav className="">
-       
+       <div className="">
+        <Link to={'/'} smooth={true} spy={true} duration={300} >shop</Link>
+       </div>
+       <div className="">
+       <Link to={'home'} smooth={true} spy={true} duration={500} className="">blog</Link>
+       <Link to={'home'} smooth={true} spy={true} duration={500} className="">shop</Link>
+       <Link to={'home'} smooth={true} spy={true} duration={500} className="">contact</Link>
+       <Link to={'home'} smooth={true} spy={true} duration={500} className="">products</Link>
+       </div>
+       <div className="">
+
+       </div>
+       <button className="" onClick={toggleMenu}>
+        
+       </button>
     </nav>
     
     </>
