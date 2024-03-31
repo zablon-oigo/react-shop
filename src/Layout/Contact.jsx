1110bb3b02c0c26cc4be8d9e27549cc3a4bc22c3
zablon <zablonoigo51@gmail.com>
2024-03-31 13:05:35 +0300

    Changed font size amd margin on top

--- src/Layout/Contact.jsx
+++ src/Layout/Contact.jsx
@@ -2,16 +2,16 @@
 function Contact() {
   return (
     <>
-    <div className="rounded-[50px] w-full  my-16 lg:h-[36rem] bg-furniture bg-no-repeat bg-cover bg-bottom  sm:p-16 px-10 py-6" data-aos="zoom-in">
-       <div className="flex flex-col justify-center w-full h-full -mt-32"
+    <div className="rounded-[50px] w-full h-[20rem] my-16  lg:h-[36rem] bg-furniture bg-no-repeat bg-cover bg-bottom  sm:p-16 px-10 py-6" data-aos="zoom-in">
+       <div className="flex flex-col justify-center w-full h-full lg:-mt-32"
        >
        <div className="">
-       <h2 className="font-semibold leading-snug text-white capitalize md:text-5xl"
+       <h2 className="text-2xl font-semibold leading-snug text-white capitalize md:text-5xl"
        data-aos="fade-right"
        data-aos-duration="2000"
        data-aos-easing="ease-in-sine"
        >subscribe and get  <span className="text-secondary">10% OFF</span>   </h2>
-        <p className="mt-3 text-sm font-semibold leading-10 text-white capitalize"
+        <p className="mt-3 font-semibold leading-10 text-white capitalize text-md lg:text-xl"
         data-aos="fade-right"
         data-aos-duration="2500"
         data-aos-easing="ease-in-sine">get 10% discount for all products</p>
